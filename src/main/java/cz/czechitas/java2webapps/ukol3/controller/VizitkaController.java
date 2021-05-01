93882d050e41d9468a1753e60e975d667e39da4c
jl <josef.lukes@gmail.com>
2021-05-01 09:16:36 +0200

    funkční přidávání a mazání vizitek

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -3,6 +3,8 @@ package cz.czechitas.java2webapps.ukol3.controller;
 import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,89 +16,66 @@ import java.util.List;
  */
 @Controller
 public class VizitkaController {
-  private final List<Vizitka> seznamVizitek;
+	private final List<Vizitka> seznamVizitek;
 
-  public VizitkaController() {
-    seznamVizitek = new ArrayList<>();
-    seznamVizitek.add(
-            new Vizitka(
-                    "Dita (Přikrylová) Formánková",
-                    "Czechitas z. s.",
-                    "Václavské náměstí 837/11",
-                    "11000 Praha 1",
-                    "dita@czechitas.cs",
-                    "+420 800123456",
-                    "www.czechitas.cz"
-            )
-    );
-    seznamVizitek.add(
-            new Vizitka(
-                    "Barbora Bühnová",
-                    "Czechitas z. s.",
-                    "Škrobárenská 511/3",
-                    "61700 Brno",
-                    null,
-                    "+420 800123456",
-                    "www.czechitas.cz"
-            )
-    );
-    seznamVizitek.add(
-            new Vizitka(
-                    "Monika Ptáčníková",
-                    "Czechitas z. s.",
-                    "Technologická 372/2",
-                    "70800 Ostrava-Pustkovec",
-                    "monika@czechitas.cs",
-                    "+420 800123456",
-                    "www.czechitas.cz"
-            )
-    );
-    seznamVizitek.add(
-            new Vizitka(
-                    "Mirka Zatloukalová",
-                    "Czechitas z. s.",
-                    "Vavrečkova 5262",
-                    "76001 Zlín",
-                    "mirka@czechitas.cs",
-                    null,
-                    "www.czechitas.cz"
-            )
-    );
-    seznamVizitek.add(
-            new Vizitka(
-                    "Ondřej Čejka",
-                    "Czechitas z. s.",
-                    "Jungmannova 3",
-                    "77900 Olomouc",
-                    null,
-                    null,
-                    "www.czechitas.cz"
-            )
-    );
-    seznamVizitek.add(
-            new Vizitka(
-                    "Kateřina Reiglová",
-                    "Czechitas z. s.",
-                    "Lipová 1789/9",
-                    "37005 České Budějovice",
-                    null,
-                    null,
-                    "www.czechitas.cz"
-            )
-    );
-  }
+	public VizitkaController() {
+		seznamVizitek = new ArrayList<>();
+		seznamVizitek.add(new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Václavské náměstí 837/11",
+				"11000 Praha 1", "dita@czechitas.cs", "+420 800123456", "www.czechitas.cz"));
+		seznamVizitek.add(new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Škrobárenská 511/3", "61700 Brno", null,
+				"+420 800123456", "www.czechitas.cz"));
+		seznamVizitek.add(new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Technologická 372/2",
+				"70800 Ostrava-Pustkovec", "monika@czechitas.cs", "+420 800123456", "www.czechitas.cz"));
+		seznamVizitek.add(new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Vavrečkova 5262", "76001 Zlín",
+				"mirka@czechitas.cs", null, "www.czechitas.cz"));
+		seznamVizitek.add(new Vizitka("Ondřej Čejka", "Czechitas z. s.", "Jungmannova 3", "77900 Olomouc", null, null,
+				"www.czechitas.cz"));
+		seznamVizitek.add(new Vizitka("Kateřina Reiglová", "Czechitas z. s.", "Lipová 1789/9", "37005 České Budějovice",
+				null, null, "www.czechitas.cz"));
+	}
 
-  @GetMapping("/")
-  public ModelAndView seznam() {
-    ModelAndView result = new ModelAndView("seznam");
-    result.addObject("seznam", seznamVizitek);
-    return result;
-  }
+	@GetMapping("/")
+	public ModelAndView seznam() {
+		ModelAndView result = new ModelAndView("seznam");
+		result.addObject("seznam", seznamVizitek);
+		return result;
+	}
 
-  @GetMapping(path = "/detail", params = "id")
-  public ModelAndView detail(int id) {
-    ModelAndView result = new ModelAndView("detail");
-    result.addObject("vizitka", seznamVizitek.get(id));
-    return result;
-  }
+	@GetMapping(path = "/detail", params = "id")
+	public ModelAndView detail(int id) {
+		ModelAndView result = new ModelAndView("detail");
+		result.addObject("vizitka", seznamVizitek.get(id));
+		result.addObject("id", id);
+		return result;
+	}
+
+	@GetMapping(path = "/nova")
+	public ModelAndView novastranka() {
+		ModelAndView result = new ModelAndView("nova");
+		//result.addObject("vizitka",seznamVizitek.);
+		return result;
+	}
+
+	@PostMapping(path = "/nova")
+	public String nova(Vizitka vizitka) {
+		seznamVizitek.add(vizitka);
+
+		//ModelAndView result = new ModelAndView("nova");
+
+		// result.addObject("vizitka", seznamVizitek.get());
+
+		return "redirect:/";
+	}
+
+	@PostMapping(path = "/detail", params = "id")
+	public String smaz(@RequestParam int id) {
+		seznamVizitek.remove(id);
+
+		// ModelAndView result = new ModelAndView("nova");
+
+		// result.addObject("vizitka", seznamVizitek.get());
+
+		return "redirect:/";
+	}
+
 }
